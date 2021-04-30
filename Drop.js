77d79dd45682b505b663b13899281270a86537fb
Stickman375 <75806815+Stickman375@users.noreply.github.com>
2021-04-29 15:13:36 -1000

    Add files via upload

---
+++ Drop.js
@@ -0,0 +1,29 @@
+class Drop{
+    constructor(x,y){
+        var options = {           
+            restitution:0.1,
+            friction:0.1
+        }
+       
+        this.drop = Bodies.circle(x,y,3.5,options);
+        this.radius = 5;
+		this.x = x;
+		this.y = y;
+        World.add(world, this.drop);
+        
+      
+    }
+    display(){
+       var pos = this.drop.position;
+    
+        ellipseMode(CENTER);
+        fill("blue");
+		
+        ellipse(pos.x,pos.y,this.radius,this.radius);
+    }
+    update(){
+        if(this.drop.position.y > height){
+            Matter.Body.setPosition(this.drop,{x:random(0,400),y:random(0,400)});
+        }
+    }
+};
